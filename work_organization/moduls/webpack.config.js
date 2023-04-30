8c3760488792bbcf9f22ffda0d4c315295883e56
PVladimirS <volodia.pavloff201343@gmail.com>
2023-04-30 09:32:32 +0300

    edited webpack.config

--- work_organization/moduls/webpack.config.js
+++ work_organization/moduls/webpack.config.js
@@ -1,3 +1,23 @@
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' 
+        }),
+        new MiniCssExtractPlugin()
+    ]
 }
